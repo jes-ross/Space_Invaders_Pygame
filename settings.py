1d78ea74f65f1363bb9596d0f3452a992187867a
unknown <jesusrosalessantana@gmail.com>
2024-03-01 09:57:15 +0000

    Cambios

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ about the game and others.
 class Settings: #A class to save all the game configuration.
 
     def __init__(self):#Initialize the game configuration.
-
+        
         self.screen_width:int = 1200
         self.screen_height:int = 800
         self.bg_color:tuple = (255, 255, 255)
