3cba796a72324009ceffe92e0a68cc663fa7c38d
unknown <jesusrosalessantana@gmail.com>
2024-03-03 21:01:13 +0000

    Añadidos todos los aliens

--- Space_invaders.py
+++ Space_invaders.py
@@ -36,16 +36,24 @@ class AlienInvasion:#Creating the game class.
         avialable_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = avialable_space_x // (2 * alien_width)
 
-        for alien_number in range(number_aliens_x):#Create first line of aliens
-            self._create_alien(alien_number)
+        #Number of lines
+        ship_height = self.ship.rect.height
+        avialable_space_y = (self.settings.screen_height - 
+                             (3 * alien_height) - ship_height)
+        number_rows = avialable_space_y // (2 * alien_height)
+
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):#Create first line of aliens
+                self._create_alien(alien_number, row_number)
 
 
 
-    def _create_alien(self, alien_number):#Create alien and add it to a line
+    def _create_alien(self, alien_number, row_number):#Create alien and add it to a line
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
 
